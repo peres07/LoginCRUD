5171e9a528ed75256e09091da606174003538aa2
José Luís <peressousa77@gmail.com>
2023-07-16 17:05:01 -0300

    delete use auth

--- frontend/src/context/hooks/useAuth.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../AuthProvider';
-
-export const useAuth = () => {
-    const context = useContext(AuthContext);
-    return context;
-};
