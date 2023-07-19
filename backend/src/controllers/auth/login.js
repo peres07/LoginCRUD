74fc52da9c391698ea540e913e016bc73553e089
José Luís <peressousa77@gmail.com>
2023-07-19 01:18:39 -0300

    change atob to buffer

--- backend/src/controllers/auth/login.js
+++ backend/src/controllers/auth/login.js
@@ -3,7 +3,7 @@ import * as db from '../../db/index.js';
 
 export async function login(req, res) {
     const hash = req.headers.authorization.split(' ')[1];
-    const [email, password] = atob(hash).split(':');
+    const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
     const user = await db.findLogin(email, password);
     if (user) {
         const token = jsonwebtoken.sign(
